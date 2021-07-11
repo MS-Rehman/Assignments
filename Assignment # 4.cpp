51c10825806339b7baa148ef64918bbfedef29d4
MS-Rehman <87252274+MS-Rehman@users.noreply.github.com>
2021-07-11 11:08:38 +0500

    Add files via upload

---
+++ Assignment # 4.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std ;
+
+main()
+
+{
+						cout << " \t\t-----( MS Rehman Assignment # 4 )-----"  ;
+	int Roll_No ;
+	float height ;
+	double Age ;
+	char section ;
+	string name ;
+	
+	cout << " \n Write Your Name : " ;
+	cin >> name ;
+	
+	
+	cout << " Write Your Class Section (A,B,C) : " ;
+	cin >> section ;
+	
+	
+	cout << " Write Your Roll no. : " ;
+	cin >> Roll_No ;
+	
+	
+	cout << " Write Your height : "  ;
+	cin >> height ;
+	
+	
+	cout << " Write Your Age According to (Years.hours)(20.1234567) : "  ;
+	cin >> Age ;
+	
+	
+					cout << " \t\t-----( Student Data )-----"  ;
+	
+	cout << " \n Student  Name   is : " <<name ;
+	cout << " \n Student Section is : " <<section  ;
+	cout << " \n Student Roll No.is : " <<Roll_No  ;
+	cout << " \n Student Height  is : " <<height  ;
+	cout << " \n Student   Age   is : " <<Age  ;
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
